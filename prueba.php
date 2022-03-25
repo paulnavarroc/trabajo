9a2b0983e54d307911cf03c9664eda54842b2824
paulnavarroc <102325540+paulnavarroc@users.noreply.github.com>
2022-03-25 17:43:44 -0500

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -29,7 +29,7 @@ $conn = mysqli_init();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grupo 5</title>
+    <title>trabajo</title>
 </head>
 <body>
     <table border="1">
